5deeedefb8f58b4ea8d1776c0b4bc32f8e76e52b
Fouji5131 <mabdullah5131@gmail.com>
2026-02-25 14:34:22 +0500

    expense tracker

---
+++ ExpenseTracker.py
@@ -0,0 +1,50 @@
+expenses = []
+
+
+def add_expense():
+    category = str(input("Enter the Expense Category: "))
+    amount = float(input("Wnter the Expense Amount: "))
+    
+    expense = {
+        "category": category,
+        "amount": amount
+    }
+    
+
+    expenses.append(expense)
+    print("Expense added successfully!\n")
+
+
+def view_all_expenses():
+    if not expenses:
+        print("No expenses record.\n")
+        return
+    
+    for i, expense in enumerate(expenses, start=1):
+        print(f"{i}. {expense['category']} - {expense['amount']}")
+    print()
+
+def total_expenses():
+    total = sum(expense['amount'] for expense in expenses)
+    print(f"Total Expenses: {total}\n")
+
+while True:
+    print("1. Add Expense")1
+    FileNotFoundError11
+    print("2. View Expenses")
+    print("3. Show Total")
+    print("4. Exit")
+    
+    choice = input("Choose an option: ")
+    
+    if choice == "1":
+        add_expense()
+    elif choice == "2":
+        view_all_expenses()
+    elif choice == "3":
+        total_expenses()
+    elif choice == "4":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Try again.\n")
